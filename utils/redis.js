f56a3b7551d710755b390648a1cadd9ebbbaefa7
kennyChukwuebuka <kenkinado@gmail>
2024-06-15 17:19:35 +0100

    added the redis task

---
+++ utils/redis.js
@@ -0,0 +1,51 @@
+const redis = require('redis');
+
+class RedisClient {
+  constructor () {
+    this.client = redis.createClient();
+
+    this.client.on('error', (err) => {
+      console.log('Redis error:', err);
+    });
+  }
+
+  async isAlive () {
+    try {
+      await this.client.ping();
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async get (key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) reject(err);
+        else resolve(value);
+      });
+    });
+  }
+
+  async set (key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (err, reply) => {
+        if (err) reject(err);
+        else resolve(reply);
+      });
+    });
+  }
+
+  async del (key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, count) => {
+        if (err) reject(err);
+        else resolve(count);
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+module.exports = redisClient;
