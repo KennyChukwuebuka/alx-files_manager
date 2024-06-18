b83390b21da891e9bbcaf9323c216c0ee535abb6
kennyChukwuebuka <kenkinado@gmail>
2024-06-18 21:23:35 +0100

    fix

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -155,6 +155,8 @@ class FilesController {
       if (!userId) return res.status(401).json({ error: 'Unauthorized' });
 
       const fileId = req.params.id;
+      if (!ObjectId.isValid(fileId)) return res.status(404).json({ error: 'Not found' });
+
       const file = await dbClient.db.collection('files').findOne({ _id: new ObjectId(fileId), userId: new ObjectId(userId) });
       if (!file) return res.status(404).json({ error: 'Not found' });
 
@@ -164,9 +166,18 @@ class FilesController {
       );
 
       const updatedFile = await dbClient.db.collection('files').findOne({ _id: new ObjectId(fileId), userId: new ObjectId(userId) });
-      return res.status(200).json(updatedFile);
+      const responseFile = {
+        id: updatedFile._id.toString(),
+        userId: updatedFile.userId.toString(),
+        name: updatedFile.name,
+        type: updatedFile.type,
+        isPublic: updatedFile.isPublic,
+        parentId: updatedFile.parentId === 0 ? 0 : updatedFile.parentId.toString(),
+      };
+
+      return res.status(200).json(responseFile);
     } catch (error) {
-      console.error('Error in publishFile:', error);
+      console.error('Error in putPublish:', error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
@@ -180,6 +191,8 @@ class FilesController {
       if (!userId) return res.status(401).json({ error: 'Unauthorized' });
 
       const fileId = req.params.id;
+      if (!ObjectId.isValid(fileId)) return res.status(404).json({ error: 'Not found' });
+
       const file = await dbClient.db.collection('files').findOne({ _id: new ObjectId(fileId), userId: new ObjectId(userId) });
       if (!file) return res.status(404).json({ error: 'Not found' });
 
@@ -189,9 +202,18 @@ class FilesController {
       );
 
       const updatedFile = await dbClient.db.collection('files').findOne({ _id: new ObjectId(fileId), userId: new ObjectId(userId) });
-      return res.status(200).json(updatedFile);
+      const responseFile = {
+        id: updatedFile._id.toString(),
+        userId: updatedFile.userId.toString(),
+        name: updatedFile.name,
+        type: updatedFile.type,
+        isPublic: updatedFile.isPublic,
+        parentId: updatedFile.parentId === 0 ? 0 : updatedFile.parentId.toString(),
+      };
+
+      return res.status(200).json(responseFile);
     } catch (error) {
-      console.error('Error in unpublishFile:', error);
+      console.error('Error in putUnpublish:', error);
       return res.status(500).json({ error: 'Internal Server Error' });
     }
   }
